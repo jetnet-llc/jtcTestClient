04bf6288f86d896f6dea358246e313ec4ec68a7c
mike-jetnet-com <mike@jetnet.com>
2023-01-21 06:56:59 -0500

    sample customer API interface

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 ﻿using jtcTestClient;
 using Newtonsoft.Json;
-using System.Text.Json.Serialization;
+using System.ComponentModel.Design;
+using System.Security.AccessControl;
+using System;
+using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices.JavaScript;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 
 if (args.Length > 0)
 {
@@ -20,8 +26,6 @@ string bearerToken = string.Empty;
 
 const string TEST_API_BASE = "https://testcustomer.jetnetconnect.com/api/"; // test sales api
 const string LIVE_API_BASE = "https://customer.jetnetconnect.com/api/"; // test sales api
-//const string LOCAL_API_BASE = "https://www.jetnetcustomer.com/api/"; // local utility api (IIS)
-//const string LOCAL_API_BASE = "https://localhost:54501/api/"; // local customer api (IISExpress)
 
 string apiBase = ""; // base api path 
 
@@ -46,9 +50,11 @@ string getContactList = contactControler + "/getContactList/{0}";
 
 Console.WriteLine("Connect To API");
 
-ApiUser loginUser = new ApiUser();
-loginUser.EmailAddress = @"demo@jetnet.com";
-loginUser.Password = @"g846ii2v";
+ApiUser loginUser = new()
+{
+  EmailAddress = @"demo@jetnet.com",
+  Password = @"g846ii2v"
+};
 
 // login to api example
 responseAPILogin loginResponse = new();
@@ -140,6 +146,120 @@ if (returnValue is not null)
 
 // get aircraft list example
 
+tmpString = string.Format(getAircraftList, accessToken.Trim());
+restURL = apiBase + tmpString;
+
+AcListOptions content = new()
+{
+  airframetype = eAirFrameTypes.None,
+  maketype = eMakeTypes.None,
+  sernbr = "",
+  regnbr = "",
+  modelid = 0,
+  makename = "ASTRA",
+  isforsale = eYesNoIgnoreFlag.Ignore,
+  lifecycle = eLifeCycle.None,
+  basestate = null,
+  basestatename = null,
+  basecountry = "",
+  basecode = "",
+  actiondate = "",
+  companyid = 0,
+  contactid = 0,
+  yearmfr = 0,
+  yeardlv = 0,
+  aircraftchanges = false,
+  aclist = null,
+  modlist = null
+};
+
+returnValue = customerAPI.GetFromAPI(bearerToken, restURL, content, "PUT").Result;
+responseAircraftList aircraftList = new();
+
+if (returnValue is not null)
+{
+  aircraftList = JsonConvert.DeserializeObject<responseAircraftList>(returnValue)!;
+
+  if (aircraftList.responsestatus!.ToUpper().Contains(@"SUCCESS"))
+    Console.WriteLine(@"aircraft list {0}", returnValue.Trim());
+  else
+    Console.WriteLine(@"ERROR : aircraft list {0}", aircraftList.responsestatus!.Trim());
+
+}
+
 // get copmpany list example
 
+tmpString = string.Format(getCompanyList, accessToken.Trim());
+restURL = apiBase + tmpString;
+
+List<string> tmpState = new();
+tmpState.Add("NY");
+tmpState.Add("NJ");
+tmpState.Add("PA");
+
+CompListOptions content1 = new()
+{
+  aircraftid = null,
+  name = "",
+  country = "",
+  city = "",
+  state = tmpState,
+  statename = null,
+  bustype = null,
+  airframetype = eAirFrameTypes.None,
+  maketype = eMakeTypes.None,
+  modelid = null,
+  makename = null,
+  relationship = null,
+  isoperator = eYesNoIgnoreFlag.Ignore,
+  actiondate = "",
+  companychanges = false,
+  website = "",
+  complist = null
+};
+
+returnValue = customerAPI.GetFromAPI(bearerToken, restURL, content1, "PUT").Result;
+responseCompanyList companyList = new();
+
+if (returnValue is not null)
+{
+  companyList = JsonConvert.DeserializeObject<responseCompanyList>(returnValue)!;
+
+  if (companyList.responsestatus!.ToUpper().Contains(@"SUCCESS"))
+    Console.WriteLine(@"company list {0}", returnValue.Trim());
+  else
+    Console.WriteLine(@"ERROR : company list {0}", companyList.responsestatus!.Trim());
+
+}
 // get contact list example
+
+tmpString = string.Format(getContactList, accessToken.Trim());
+restURL = apiBase + tmpString;
+
+ContListOptions content2 = new()
+{
+  aircraftid = null,
+  companyid = 0,
+  companyname = "JETNET",
+  firstname = "",
+  lastname = "",
+  title = "",
+  email = "",
+  actiondate = "",
+  phonenumber = "",
+  contactchanges = false,
+  contlist = null
+};
+
+returnValue = customerAPI.GetFromAPI(bearerToken, restURL, content2, "PUT").Result;
+responseContactList contactList = new();
+
+if (returnValue is not null)
+{
+  contactList = JsonConvert.DeserializeObject<responseContactList>(returnValue)!;
+
+  if (contactList.responsestatus!.ToUpper().Contains(@"SUCCESS"))
+    Console.WriteLine(@"contact list {0}", returnValue.Trim());
+  else
+    Console.WriteLine(@"ERROR : contact list {0}", contactList.responsestatus!.Trim());
+}
